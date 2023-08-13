17237f7a3ba4297ee5c73e3b75ecc41da60de57a
fatmanuryikilmaz <125798606+fatmanuryikilmaz@users.noreply.github.com>
2023-08-13 23:49:59 +0300

    frontend updated

--- Ecommerce/Ecommerce/Controllers/AdminController.cs
+++ Ecommerce/Ecommerce/Controllers/AdminController.cs
@@ -21,7 +21,7 @@ namespace Ecommerce.Controllers
         public IActionResult Index(Admin admin)
         {
             var newEntrance=_context.Admins.Where(x=>x.AdminName == admin.AdminName && x.AdminPassword==admin.AdminPassword).FirstOrDefault();
-            // var entrance = from x in _context.Admins where admin.AdminName == x.AdminName && admin.AdminPassword == x.AdminPassword select x;entrance.Count() > 0)
+           
             if (newEntrance==null)
             {
                 return RedirectToAction("Index", "Admin");
@@ -31,14 +31,6 @@ namespace Ecommerce.Controllers
                 return RedirectToAction("Index", "Category");
             }
         }
-        //public IActionResult Home()
-        //{
-        //    return View();
-        //}
-        //[HttpPost]
-        //public IActionResult Home(Admin admin)
-        //{
-        //    return View("Index", "Admin");
-        //}
+
     }
 }
--- Ecommerce/Ecommerce/Controllers/BasketController.cs
+++ Ecommerce/Ecommerce/Controllers/BasketController.cs
@@ -8,5 +8,6 @@ namespace Ecommerce.Controllers
         {
             return View();
         }
+
     }
 }
